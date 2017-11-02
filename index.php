81b8b294f333ae1635b299f7ccf471303a3a21c8
Zhuo Ning <zning3@my.bcit.ca>
2017-11-01 18:10:31 -0700

    almost done

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
-<?php include("include/header.php");?>
+<?php 
+session_start();
+include("include/header.php");
+extract($_POST);
+extract($_GET);
+if(!isset($answer)){
+    $score=0;
+    $count=0;
+}
+if(isset($answer)){
+    $count = $lastCount;
+    $score = $lastScore;
+    if(is_numeric($answer)){
+        if($answer == $lastResult){
+            $score++;
+            $count++;
+        } else {
+            $validation = "INCORRECT, $lastFirst $lastSign $lastSecond is $lastResult";
+            $count++;
+        }
+    } else {
+        $validation = "You must enter a number for your answer.";
+    }
+}
+$first = rand(0,50);
+$second = rand(0,50);
+$operator = rand(0,1);
+$result = 0;
+$sign = "";
+if ($operator==0){
+    $result = $first + $second;
+    $sign = "+";
+} else {
+    $result = $first - $second;
+    $sign = "-";
+}
+?>
 <div class="row">
     <form action="authenticate.php" method="post" class="form-horizontal">
         <div class="col-sm-3 col-sm-offset-9">
@@ -8,5 +44,43 @@
             <button type="submit" class="bth bth-default">Logout</button>
         </div>
     </form>
-    <div class="col-sm-10 col-sm-offset-2"><h1>Math game</h1></div>
+    <div class="col-sm-8 col-sm-offset-4"><h1>Math game</h1></div>
+</div>
+<div class="row">
+    <form action="index.php" method="post" class="form-horizontal">
+        <div class="col-lg-12 row">
+            <div class="col-xs-1 col-sm-offset-4"><?php echo $first; ?></div>
+            <div class="col-xs-1"><?php echo $sign; ?></div>
+            <div class="col-xs-1"><?php echo $second; ?></div>
+            <div class="col-xs-5"></div>
+        </div>
+        <div class="col-lg-12 row">
+            <div class="col-sm-4 col-sm-offset-4">
+                <input type="text" class="form-control" id="answer" name="answer" 
+                placeholder="Enter answer" size="6" />
+                <input type="hidden" id="lastResult" name="lastResult" value="<?php echo $result; ?>"/>
+                <input type="hidden" id="lastFirst" name="lastFirst" value="<?php echo $first; ?>"/>
+                <input type="hidden" id="lastSecond" name="lastSecond" value="<?php echo $second; ?>"/>
+                <input type="hidden" id="lastSign" name="lastSign" value="<?php echo $sign; ?>"/>
+                <input type="hidden" id="lastScore" name="lastScore" value="<?php echo $score; ?>"/>
+                <input type="hidden" id="lastCount" name="lastCount" value="<?php echo $count; ?>"/>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+        <div class="col-lg-12 row">
+            <div class="col-sm-3 col-sm-offset-4">
+                <button type="submit" class="bth bth-default">Submit</button>
+            </div>
+            <div class="col-sm-5"></div>
+        </div>
+    </form>
+    <div class="col-lg-12 row"><hr/></div>
+    <div class="col-lg-12 row">
+        <div class="col-sm-6 col-sm-offset-4 error"><?php echo $validation; ?></div>
+        <div class="col-sm-2"></div>
+    </div>
+    <div class="col-lg-12 row">
+        <div class="col-sm-6 col-sm-offset-4">Score: <?php echo "$score/$count"; ?></div>
+        <div class="col-sm-2"></div>
+    </div>
 </div>
